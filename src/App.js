7b17f33412e6312fa74ac9dadf574c8ce786370e
ChEmunah <chanamuna1@gmail.com>
2019-04-02 13:16:46 +0300

    choos shape

--- src/App.js
+++ src/App.js
@@ -3,47 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       color: "",
-      shape:""
+      shape: ""
     }
   }
-  pickColor = (color) =>{
-    this.setState({color: color},()=>console.log("color: ", this.state.color));
-
-    
-
+  pickColor = (color) => {
+    this.setState({ color: color }, () => console.log("color: ", this.state.color));
   }
-render() {
-  let colors = {blue:"#1653b7", green:"#22821b", red:"#3a3838", yello: "#eef209"};
-  let shapes = ["triangle", "square", "halfCircle","rectangle"];
+  pickShape = (shape) => {
+    this.setState({ shape: shape }, () => console.log("shape: ", this.state.shape));
+  }
+  render() {
+    let colors = { blue: "#1653b7", green: "#22821b", red: "#3a3838", yello: "#eef209" };
+    let shapes = ["triangle", "square", "halfCircle", "rectangle"];
     return (
       <div className="App">
-      {/* choos color */}
-      <h1>בחר צבע</h1>
-      <div className = {"colorPicker"}>
-        {
-          Object.keys(colors).map(item => {
-            return <div onClick ={() => this.pickColor(colors[item])} 
-            id = {`choosColor${item}`} 
-            className = "choosColor"
-            key = {item}
-            ref = {`choosColor${item}`}
-            style={{backgroundColor:colors[item]}}
-            >{item} <span aria-hidden="true" data-icon="&#x25a8;"></span>
-            </div>
+        {/* choos color */}
+        <h1>בחר צבע</h1>
+        <div className={"choosParams"}>
+          {
+            Object.keys(colors).map(item => {
+              return <div onClick={() => this.pickColor(colors[item])}
+                id={`choosColor${item}`}
+                className="choosColorAndShape"
+                key={item}
+                style={{ backgroundColor: colors[item] }}
+              >{item} <span aria-hidden="true" data-icon="&#x25a8;"></span>
+              </div>
+            })
+
+          }
+        </div>
+        {/* choos shape */}
+        <h1>בחר צורה</h1>
+
+        <div className={"choosParams"}>
+          {shapes.map((shape) => {
+            return (
+              <div
+              className="choosColorAndShape"
+              onClick={() => this.pickShape(shape)}>
+                {shape}
+              </div>
+            )
           })
-          
-        }
-      </div>
-      {/* choos shape */}
-      <h1>בחר צורה</h1>
-      {
-        // shape
-      }
+          }
+        </div>
+
+
 
       </div>
     );
